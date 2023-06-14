c98ab24671e76860a4e76beab8a1491e873b0766
Paresh Patil <106147065+paresh122@users.noreply.github.com>
2023-06-14 10:00:49 +0530

    Add files via upload

---
+++ Learning/Day-15-15_demo.py
@@ -0,0 +1,11 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="abc",
+  password="password"
+)
+print(mydb)
+mycursor = mydb.cursor()
+mycursor.execute("SHOW DATABASES")
+for x in mycursor:
+  print(x)
